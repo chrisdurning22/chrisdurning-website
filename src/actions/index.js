1b1bc1dc1b4b1699cee748295b0970a444f4b9e7
Chris Durning <chrisdurning18@gmail.com>
2019-06-25 21:55:50 +0100

    changes made

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,13 +1,13 @@
 export const selectIndex = (index) => {
-return {
-		type: 'INDEX_SELECTED',
-		index: index
-	};
+  return {
+    type: 'INDEX_SELECTED',
+    index: index
+  };
 };
 
 export const selectTopic = (topic) => {
-	return {
-		type: 'TOPIC_SELECTED',
-		payload: topic
-	};
+  return {
+    type: 'TOPIC_SELECTED',
+    payload: topic
+  };
 };
--- src/components/TopicMenu.js
+++ src/components/TopicMenu.js
@@ -4,30 +4,30 @@ import { selectIndex } from '../actions';
 import { selectTopic } from '../actions';
 
 class TopicMenu extends React.Component {
-	renderMenu() {
-		return this.props.topics.map((topic, index) => {
-			const className = this.props.selectedIndex === index ? 'active item' : 'item';
-			return (
-				<div
-					className={className}
-					onClick={() => {this.props.selectIndex(index);this.props.selectTopic(topic);}}
-					>
-					{topic.title}
-				</div>
-			);
-		});
-	}
+  renderMenu() {
+    return this.props.topics.map((topic, index) => {
+      const className = this.props.selectedIndex === index ? 'active item' : 'item';
+      return (
+        <div
+          className={className}
+          onClick={() => {this.props.selectIndex(index);this.props.selectTopic(topic);}}
+        >
+          {topic.title}
+        </div>
+      );
+    });
+  }
 
-	render() {
-		return <div>{this.renderMenu()}</div>;
-	}
+  render() {
+    return <div>{this.renderMenu()}</div>;
+  }
 }
 
 const mapStateToProps = (state) => {
-	return {
-		topics: state.information,
-		selectedIndex: state.selectedIndex
-	};
+  return {
+    topics: state.information,
+    selectedIndex: state.selectedIndex
+  };
 };
 
 export default connect(mapStateToProps, { selectIndex, selectTopic })(TopicMenu);
