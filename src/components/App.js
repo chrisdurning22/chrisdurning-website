a90e7566380f2d6e14e3642ffebc6c506daf4b6b
Chris Durning <chrisdurning18@gmail.com>
2019-06-25 21:47:34 +0100

    changes made

--- src/components/App.js
+++ src/components/App.js
@@ -5,8 +5,8 @@ import profile from '../images/profile.png';
 
 
 const App = () => {
-	return (
-		<div className="ui grid">
+  return (
+    <div className="ui grid">
       <div className="four wide column">
         <div className="ui vertical fluid tabular menu">
           <TopicMenu/>
@@ -22,22 +22,21 @@ const App = () => {
         </div>
         <div style={{textAlign: `center`}}>
           <a href="https://github.com/chrisdurning22?tab=repositories" className="huge ui circular github icon button">
-          <i className="github icon"></i>
+            <i className="github icon"></i>
           </a>
           <a href="https://www.linkedin.com/in/chris-durning-9186b5169/" className="huge ui circular linkedin icon button">
-          <i className="linkedin icon"></i>
+            <i className="linkedin icon"></i>
           </a>
           <a href="https://www.instagram.com/durning_96/?hl=en" className="huge ui circular instagram icon button">
-          <i className="instagram icon"></i>
+            <i className="instagram icon"></i>
           </a>
           <a href="https://ai.stackexchange.com/users/15356/chris" className="huge ui circular stack exchange icon button">
-          <i className="stack exchange icon"></i>
+            <i className="stack exchange icon"></i>
           </a>
         </div>
       </div>
-		</div>
-	);
+    </div>
+  );
 };
 
 export default App;
-
